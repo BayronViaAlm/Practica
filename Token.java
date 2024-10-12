16af8986c61102e4da374e404d7991a412be7c3f
BayronViaAlm <bviamontealmonte@gmail.com>
2024-10-12 12:32:10 +0200

    Acciones semanticas, tabla de transiciones e inicio de codigo AL, TablaSimbolos y Token

---
+++ Token.java
@@ -0,0 +1,21 @@
+public class Token {
+    private String codigo;
+    private String atributo;
+
+    public Token (String cod, String atri) {
+        codigo = cod;
+        atributo = atri;
+    }
+
+    public String getCod (){
+        return codigo;
+    }
+
+    public String getAtr() {
+        return atributo;
+    }
+
+    public String toString() {
+        return "< " + codigo + ", " + (atributo != null? atributo : " >");
+    }
+}
